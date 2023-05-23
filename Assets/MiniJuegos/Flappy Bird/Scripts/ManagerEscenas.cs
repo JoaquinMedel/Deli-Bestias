c751c987cf95fbcde04d4490b48c0dffb7e88de0
Ciidan <j.sandovalcastrocity@gmail.com>
2023-05-22 23:31:16 -0400

    Jose
    MiniJuego 100% integrado

--- Assets/MiniJuegos/Flappy Bird/Scripts/ManagerEscenas.cs
+++ Assets/MiniJuegos/Flappy Bird/Scripts/ManagerEscenas.cs
@@ -6,21 +6,28 @@ using UnityEngine.SceneManagement;
 public class ManagerEscenas : MonoBehaviour
 {
     public GameObject gameOverCanvas;
+    public GameObject iniciandoJuego;
+    public GameObject perdiendoJuego;
 
     private void Start()
     {
+        Time.timeScale = 0;
+        gameOverCanvas.SetActive(true);
+        iniciandoJuego.SetActive(true);
+    }
+
+    public void Iniciar()
+    {
+        gameOverCanvas.SetActive(false);
+        iniciandoJuego.SetActive(false);
         Time.timeScale = 1;
     }
 
     public void GameOver()
     {
         gameOverCanvas.SetActive(true);
+        perdiendoJuego.SetActive(true);
         Time.timeScale = 0;
-    }
-
-    public void Reiniciar()
-    {
-        SceneManager.LoadScene(0);
     }
 
 
