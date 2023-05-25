94d6c52cf39110faa395957e96987f977fce8fa3
JoaquinMedel <j.medelplaza@uandresbello.edu>
2023-05-25 17:36:32 -0400

    Joaquín
    Cambio de salas NUEVAMENTE terminado, falta velocidad de cambio de camara

--- Assets/7 Scripts/ChangeCamera.cs
+++ Assets/7 Scripts/ChangeCamera.cs
@@ -11,10 +11,56 @@ public class ChangeCamera : MonoBehaviour
     private int index = 0;
     private void Start()
     {
-        foreach (CinemachineVirtualCamera c in cameras)
-        {
-            c.Priority = 0;
-        }
+       cameras[0].gameObject.SetActive(true);
+       cameras[1].gameObject.SetActive(false);
+       cameras[2].gameObject.SetActive(false);
+       cameras[3].gameObject.SetActive(false);
+       cameras[4].gameObject.SetActive(false);
+    }
+
+    public void Tienda()
+    {
+        cameras[0].gameObject.SetActive(false);
+        cameras[1].gameObject.SetActive(false);
+        cameras[2].gameObject.SetActive(false);
+        cameras[3].gameObject.SetActive(true);
+        cameras[4].gameObject.SetActive(false);
+    }
+
+    public void Minijuegos()
+    {
+        cameras[0].gameObject.SetActive(false);
+        cameras[1].gameObject.SetActive(false);
+        cameras[2].gameObject.SetActive(true);
+        cameras[3].gameObject.SetActive(false);
+        cameras[4].gameObject.SetActive(false);
+    }
+
+    public void Gacha()
+    {
+        cameras[0].gameObject.SetActive(false);
+        cameras[1].gameObject.SetActive(true);
+        cameras[2].gameObject.SetActive(false);
+        cameras[3].gameObject.SetActive(false);
+        cameras[4].gameObject.SetActive(false);
+    }
+
+    public void Clothes()
+    {
+        cameras[0].gameObject.SetActive(false);
+        cameras[1].gameObject.SetActive(false);
+        cameras[2].gameObject.SetActive(false);
+        cameras[3].gameObject.SetActive(false);
+        cameras[4].gameObject.SetActive(true);
+    }
+
+    public void Inicio()
+    {
+        cameras[0].gameObject.SetActive(true);
+        cameras[1].gameObject.SetActive(false);
+        cameras[2].gameObject.SetActive(false);
+        cameras[3].gameObject.SetActive(false);
+        cameras[4].gameObject.SetActive(false);
     }
     
 }
